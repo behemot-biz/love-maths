539557de67e0be31c75e02565d793b42a1dc83e4
Petra <petra@behemot.biz>
2024-04-30 06:49:44 +0200

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,30 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You Clicked Submit!");
             } else {let gameType = this.getAttribute("data-type");
-                alert(`You Clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  *  Main game "loop", called when script is first loaded
  *  and after the usersanswer has been processed
  */
-function runGame(){
+function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 ) + 1;
     let num2 = Math.floor(Math.random() * 25 ) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`; //writes to console
+    }
 }
 
 function checkAnswer(){
@@ -41,7 +50,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
